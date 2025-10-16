c7978c8d5b7ea027eae696a00e2986e30ffdfe78
ukduw <edhkmayo@gmail.com>
2025-10-16 09:55:31 -0400

    added dir, logging...

--- net-fallback.sh
+++ net-fallback.sh
@@ -11,4 +11,10 @@ CHECK_HOST="1.1.1.1" # reliable ip to ping (Cloudflare)
 CHECK_URL="https://cloudflare.com/robots.txt" # optional http check...
 FAIL_THRESHOLD=3
 RECOVER_THRESHOLD=3 # consider decreasing both...
-SLEEP_LOOP=15 # consider decreasing... minimum additional downtime would be 45 
+SLEEP_LOOP=15 # consider decreasing... minimum additional downtime would be 45
+
+STATE_DIR="/var/run/net-fallback"
+mkdir -p :"$STATE_DIR"
+FAIL_COUNT_FILE="$STATE_DIR/fail_count"
+OK_COUNT_FILE="$STATE_DIR/ok_count"
+
