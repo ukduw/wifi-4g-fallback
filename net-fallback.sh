01d6b7d36f372f096b8d1df1e7f73ddd368c916d
ukduw <edhkmayo@gmail.com>
2025-10-16 09:54:16 -0400

    added config

--- net-fallback.sh
+++ net-fallback.sh
@@ -5,3 +5,10 @@
 # net-fallback.sh
 # Requires: nmcli, ping, logger
 # Config:
+WIFI_CONN="home-wifi"
+MOBILE_CONN="4g-usb"
+CHECK_HOST="1.1.1.1" # reliable ip to ping (Cloudflare)
+CHECK_URL="https://cloudflare.com/robots.txt" # optional http check...
+FAIL_THRESHOLD=3
+RECOVER_THRESHOLD=3 # consider decreasing both...
+SLEEP_LOOP=15 # consider decreasing... minimum additional downtime would be 45 
